45b7d20de9df276834dc664f8306a988f9c8dc82
Kyle Trahan <ktrahan2@gmail.com>
2020-09-22 19:20:39 -0400

    login returning token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,6 +31,7 @@ class UsersController < ApplicationController
 
     def login
         @user = User.find_by(username: params[:username])
+        
         if @user && @user.authenticate(params[:password])
             @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
             render json: {user: @user, token: @token}
