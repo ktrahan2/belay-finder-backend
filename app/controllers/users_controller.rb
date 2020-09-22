895130643705e701e3bf8c974a0e20e102962e2c
Kyle Trahan <ktrahan2@gmail.com>
2020-09-22 19:12:26 -0400

    fixed signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,13 +26,12 @@ class UsersController < ApplicationController
     def update
         @user = User.find(params[:id])
         @user.update(user_params)
-        render json: @user
+        render json: @user    
     end
 
     def login
         @user = User.find_by(username: params[:username])
-        byebug
-        if @user && @user.authenticate(params[:password_digest])
+        if @user && @user.authenticate(params[:password])
             @token = JWT.encode({user_id: @user.id}, Rails.application.secrets.secret_key_base[0])
             render json: {user: @user, token: @token}
         else
@@ -43,7 +42,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params
-    params.require(:user).permit(:username, :email, :password_digest, :name, :climbing_skill, :aboutme, :climbing_style)
+    params.require(:user).permit(:username, :email, :password, :name, :climbing_skill, :aboutme)
     end
 
 
